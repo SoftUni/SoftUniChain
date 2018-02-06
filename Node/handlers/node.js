c6fc2ec100cdbdcbef72df20caa834954f4f8360
Milen Radkov <milenradkov@me.com>
2018-02-06 15:40:29 +0200

    Return blocks endpoints
    Return blocks endpoints

--- Node/handlers/node.js
+++ Node/handlers/node.js
@@ -28,94 +28,96 @@ module.exports.getNodeInfo = (req, res) => {
 
 module.exports.getNodeBlocks = (req,res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send([
-        {
-            "index": 0,
-            "transactions": [
-                {
-                    "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
-                    "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
-                    "value": 25.00,
-                    "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
-                    "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
-                    "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
-                    "paid": true,
-                    "dateReceived": "2018-02-01T07:47:51.982Z",
-                    "minedInBlockIndex": 7
-                },
-                {
-                    "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
-                    "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
-                    "value": 25.00,
-                    "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
-                    "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
-                    "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
-                    "paid": true,
-                    "dateReceived": "2018-02-01T07:47:51.982Z",
-                    "minedInBlockIndex": 7
-                }
-            ],
-            "difficulty": 5,
-            "prevBlockHash": "d279fa6a31ae4fb07cfd9cf7f35cc01f�3cf20a",
-            "minedBy": "f582d57711a618e69d588ce93895d749858fa95b",
-            "blockDataHash": "5d845cddcd4404ecfd5476fd6b1cf0e5�a80cd3",
-            "nonce": 2455432,
-            "dateCreated": "2018-02-01T23:23:56.337Z",
-            "blockHash": "00000abf2f3d86d5c000c0aa7a425a6a4a65�cf4c"
-        },
-        {
-            "index": 1,
-            "transactions": [],
-            "difficulty": 5,
-            "prevBlockHash": "d279fa6a31ae4fb07cfd9cf7f35cc01f�3cf20a",
-            "minedBy": "f582d57711a618e69d588ce93895d749858fa95b",
-            "blockDataHash": "5d845cddcd4404ecfd5476fd6b1cf0e5�a80cd3",
-            "nonce": 2455432,
-            "dateCreated": "2018-02-01T23:23:56.337Z",
-            "blockHash": "00000abf2f3d86d5c000c0aa7a425a6a4a65�cf4c"
-        }
-    ])
+    res.send(main.blockchain)
+
+        // [
+        // {
+        //     "index": 0,
+        //     "transactions": [
+        //         {
+        //             "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
+        //             "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
+        //             "value": 25.00,
+        //             "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
+        //             "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
+        //             "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
+        //             "paid": true,
+        //             "dateReceived": "2018-02-01T07:47:51.982Z",
+        //             "minedInBlockIndex": 7
+        //         },
+        //         {
+        //             "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
+        //             "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
+        //             "value": 25.00,
+        //             "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
+        //             "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
+        //             "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
+        //             "paid": true,
+        //             "dateReceived": "2018-02-01T07:47:51.982Z",
+        //             "minedInBlockIndex": 7
+        //         }
+        //     ],
+        //     "difficulty": 5,
+        //     "prevBlockHash": "d279fa6a31ae4fb07cfd9cf7f35cc01f�3cf20a",
+        //     "minedBy": "f582d57711a618e69d588ce93895d749858fa95b",
+        //     "blockDataHash": "5d845cddcd4404ecfd5476fd6b1cf0e5�a80cd3",
+        //     "nonce": 2455432,
+        //     "dateCreated": "2018-02-01T23:23:56.337Z",
+        //     "blockHash": "00000abf2f3d86d5c000c0aa7a425a6a4a65�cf4c"
+        // },
+        //     {
+        //         "index": 1,
+        //         "transactions": [],
+        //         "difficulty": 5,
+        //         "prevBlockHash": "d279fa6a31ae4fb07cfd9cf7f35cc01f�3cf20a",
+        //         "minedBy": "f582d57711a618e69d588ce93895d749858fa95b",
+        //         "blockDataHash": "5d845cddcd4404ecfd5476fd6b1cf0e5�a80cd3",
+        //         "nonce": 2455432,
+        //         "dateCreated": "2018-02-01T23:23:56.337Z",
+        //         "blockHash": "00000abf2f3d86d5c000c0aa7a425a6a4a65�cf4c"
+        //     }
+        // ]
 }
 
 module.exports.getNodeBlockByIndex = (req,res) => {
     let index = req.params.index;
     res.setHeader('Content-Type', 'application/json');
-    res.send(
-        {
-            "index":index,
-            "transactions": [
-                {
-                    "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
-                    "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
-                    "value": 25.00,
-                    "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
-                    "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
-                    "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
-                    "paid": true,
-                    "dateReceived": "2018-02-01T07:47:51.982Z",
-                    "minedInBlockIndex": 7
-                },
-                {
-                    "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
-                    "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
-                    "value": 25.00,
-                    "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
-                    "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
-                    "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
-                    "paid": true,
-                    "dateReceived": "2018-02-01T07:47:51.982Z",
-                    "minedInBlockIndex": 7
-                }
-            ],
-            "difficulty": 5,
-            "prevBlockHash": "d279fa6a31ae4fb07cfd9cf7f35cc01f�3cf20a",
-            "minedBy": "f582d57711a618e69d588ce93895d749858fa95b",
-            "blockDataHash": "5d845cddcd4404ecfd5476fd6b1cf0e5�a80cd3",
-            "nonce": 2455432,
-            "dateCreated": "2018-02-01T23:23:56.337Z",
-            "blockHash": "00000abf2f3d86d5c000c0aa7a425a6a4a65�cf4c"
-        }
-    )
+    res.send(main.blockchain[index]);
+
+    // {
+    //     "index":index,
+    //     "transactions": [
+    //     {
+    //         "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
+    //         "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
+    //         "value": 25.00,
+    //         "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
+    //         "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
+    //         "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
+    //         "paid": true,
+    //         "dateReceived": "2018-02-01T07:47:51.982Z",
+    //         "minedInBlockIndex": 7
+    //     },
+    //     {
+    //         "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
+    //         "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
+    //         "value": 25.00,
+    //         "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
+    //         "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
+    //         "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
+    //         "paid": true,
+    //         "dateReceived": "2018-02-01T07:47:51.982Z",
+    //         "minedInBlockIndex": 7
+    //     }
+    // ],
+    //     "difficulty": 5,
+    //     "prevBlockHash": "d279fa6a31ae4fb07cfd9cf7f35cc01f�3cf20a",
+    //     "minedBy": "f582d57711a618e69d588ce93895d749858fa95b",
+    //     "blockDataHash": "5d845cddcd4404ecfd5476fd6b1cf0e5�a80cd3",
+    //     "nonce": 2455432,
+    //     "dateCreated": "2018-02-01T23:23:56.337Z",
+    //     "blockHash": "00000abf2f3d86d5c000c0aa7a425a6a4a65�cf4c"
+    // }
 }
 
 module.exports.getNodeBalanceByAddress = (req,res) => {
