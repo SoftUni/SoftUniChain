57418e1d60908a715e1f34070c04d3d24e142b08
Svetlin Nakov <github@nakov.com>
2018-02-10 19:55:06 +0200

    Added date & time in the transactions JSON (to avoid replay attacks)

--- Wallet-Python/blockchain-address.py
+++ Wallet-Python/blockchain-address.py
@@ -47,7 +47,8 @@ def existing_private_key_to_address(private_key_hex):
     print("blockchain address:", addr)
 
 
-def sign_transaction(recipient_address: str, value: int, sender_priv_key_hex: str):
+def sign_transaction(recipient_address: str, value: int, dateCreated: str,
+                     sender_priv_key_hex: str):
     print("Generate and sign a transaction")
     print("-------------------------------")
 
@@ -59,13 +60,19 @@ def sign_transaction(recipient_address: str, value: int, sender_priv_key_hex: st
     sender_address = ripemd160(pub_key_compressed)
     print("sender address:", sender_address)
 
-    tran = {'from': sender_address, 'to': recipient_address,
-            'value': value, 'senderPubKey': pub_key_compressed}
+    tran = {
+        'from': sender_address,
+        'to': recipient_address,
+        'value': value,
+        'dateCreated': dateCreated,
+        'senderPubKey': pub_key_compressed
+    }
     json_encoder = json.JSONEncoder(separators=(',',':'))
     tran_json = json_encoder.encode(tran)
     print("transaction (json):", tran_json)
     tran_hash = sha256(tran_json)
     print("transaction hash (sha256):", hex(tran_hash)[2:])
+
     tran_signature = sign(generator_secp256k1, sender_priv_key, tran_hash)
     print("transaction signature:", tran_signature)
     tran['senderSignature'] = [hex(tran_signature[0])[2:], hex(tran_signature[1])[2:]]
@@ -76,12 +83,10 @@ def sign_transaction(recipient_address: str, value: int, sender_priv_key_hex: st
 random_private_key_to_address()
 
 print()
-
-existing_private_key_to_address(
-    "7e4670ae70c98d24f3662c172dc510a085578b9ccc717e6c2f4e547edd960a34")
+existing_private_key_to_address("7e4670ae70c98d24f3662c172dc510a085578b9ccc717e6c2f4e547edd960a34")
 
 print()
-
 sign_transaction(
     recipient_address="f51362b7351ef62253a227a77751ad9b2302f911", value=25,
+    dateCreated='2018-02-10T17:53:48.972Z',
     sender_priv_key_hex="7e4670ae70c98d24f3662c172dc510a085578b9ccc717e6c2f4e547edd960a34")
