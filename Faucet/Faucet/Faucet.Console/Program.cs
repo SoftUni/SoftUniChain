226ff7378169e726c6ecac4a0fa9fc7d632b3559
nikolay_eml <nikolay@fresh-range.com>
2018-02-11 11:12:19 +0200

    Sign Transaction Ready

---
+++ Faucet/Faucet/Faucet.Console/Program.cs
@@ -0,0 +1,202 @@
+﻿
+
+namespace Faucet.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System;
+    using System.Text;
+    using System.Linq;
+    using Org.BouncyCastle.Asn1.X9;
+    using Org.BouncyCastle.Asn1.Sec;
+    using Org.BouncyCastle.Crypto.Parameters;
+    using Org.BouncyCastle.Math.EC;
+    using Org.BouncyCastle.Crypto;
+    using Org.BouncyCastle.Crypto.Generators;
+    using Org.BouncyCastle.Security;
+    using Org.BouncyCastle.Math;
+    using Org.BouncyCastle.Crypto.Digests;
+    using Newtonsoft.Json;
+    using Org.BouncyCastle.Asn1.Ocsp;
+    using Org.BouncyCastle.Crypto.Signers;
+    class Program
+    {
+        static readonly X9ECParameters curve = SecNamedCurves.GetByName("secp256k1");
+
+        public static ECPoint GetPublicKeyFromPrivateKey(BigInteger privKey)
+        {
+            ECPoint pubKey = curve.G.Multiply(privKey).Normalize();
+            return pubKey;
+        }
+
+        public static AsymmetricCipherKeyPair GenerateRandomKeys(int keySize = 256)
+        {
+            ECKeyPairGenerator gen = new ECKeyPairGenerator();
+            SecureRandom secureRandom = new SecureRandom();
+            KeyGenerationParameters keyGenParam =
+                new KeyGenerationParameters(secureRandom, keySize);
+            gen.Init(keyGenParam);
+            return gen.GenerateKeyPair();
+        }
+
+        public static string BytesToHex(byte[] bytes)
+        {
+            return string.Concat(bytes.Select(b => b.ToString("x2")));
+        }
+
+        public static string EncodeECPointHexCompressed(ECPoint point)
+        {
+            BigInteger x = point.XCoord.ToBigInteger();
+            return x.ToString(16) + Convert.ToInt32(!x.TestBit(0));
+        }
+
+        private static string CalcRipeMD160(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            RipeMD160Digest digest = new RipeMD160Digest();
+            digest.BlockUpdate(bytes, 0, bytes.Length);
+            byte[] result = new byte[digest.GetDigestSize()];
+            digest.DoFinal(result, 0);
+            return BytesToHex(result);
+        }
+
+        private static byte[] CalcSHA256(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            Sha256Digest digest = new Sha256Digest();
+            digest.BlockUpdate(bytes, 0, bytes.Length);
+            byte[] result = new byte[digest.GetDigestSize()];
+            digest.DoFinal(result, 0);
+            return result;
+        }
+
+        private static void RandomPrivateKeyToAddress()
+        {
+            Console.WriteLine("Random private key --> public key --> address");
+            Console.WriteLine("---------------------------------------------");
+
+            var keyPair = GenerateRandomKeys();
+
+            BigInteger privateKey = ((ECPrivateKeyParameters)keyPair.Private).D;
+            Console.WriteLine("Private key (hex): " + privateKey.ToString(16));
+            Console.WriteLine("Private key: " + privateKey.ToString(10));
+
+            ECPoint pubKey = ((ECPublicKeyParameters)keyPair.Public).Q;
+            Console.WriteLine("Public key: ({0}, {1})",
+                pubKey.XCoord.ToBigInteger().ToString(10),
+                pubKey.YCoord.ToBigInteger().ToString(10));
+
+            string pubKeyCompressed = EncodeECPointHexCompressed(pubKey);
+            Console.WriteLine("Public key (compressed): " + pubKeyCompressed);
+
+            string addr = CalcRipeMD160(pubKeyCompressed);
+            Console.WriteLine("Blockchain address: " + addr);
+        }
+
+        private static void ExistingPrivateKeyToAddress(string privKeyHex)
+        {
+            Console.WriteLine("Existing private key --> public key --> address");
+            Console.WriteLine("-----------------------------------------------");
+
+            BigInteger privateKey = new BigInteger(privKeyHex, 16);
+            Console.WriteLine("Private key (hex): " + privateKey.ToString(16));
+            Console.WriteLine("Private key: " + privateKey.ToString(10));
+
+            ECPoint pubKey = GetPublicKeyFromPrivateKey(privateKey);
+            Console.WriteLine("Public key: ({0}, {1})",
+                pubKey.XCoord.ToBigInteger().ToString(10),
+                pubKey.YCoord.ToBigInteger().ToString(10));
+
+            string pubKeyCompressed = EncodeECPointHexCompressed(pubKey);
+            Console.WriteLine("Public key (compressed): " + pubKeyCompressed);
+
+            string addr = CalcRipeMD160(pubKeyCompressed);
+            Console.WriteLine("Blockchain address: " + addr);
+        }
+
+        /// <summary>
+        /// Calculates deterministic ECDSA signature (with HMAC-SHA256), based on secp256k1 and RFC-6979.
+        /// </summary>
+        private static BigInteger[] SignData(BigInteger privateKey, byte[] data)
+        {
+            ECDomainParameters ecSpec = new ECDomainParameters(curve.Curve, curve.G, curve.N, curve.H);
+            ECPrivateKeyParameters keyParameters = new ECPrivateKeyParameters(privateKey, ecSpec);
+            IDsaKCalculator kCalculator = new HMacDsaKCalculator(new Sha256Digest());
+            ECDsaSigner signer = new ECDsaSigner(kCalculator);
+            signer.Init(true, keyParameters);
+            BigInteger[] signature = signer.GenerateSignature(data);
+            return signature;
+        }
+
+        private static void CreateAndSignTransaction(string recipientAddress, int value,
+            string iso8601datetime, string senderPrivKeyHex)
+        {
+            Console.WriteLine("Generate and sign a transaction");
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine("Sender private key:", senderPrivKeyHex);
+            BigInteger privateKey = new BigInteger(senderPrivKeyHex, 16);
+
+            ECPoint pubKey = GetPublicKeyFromPrivateKey(privateKey);
+            string senderPubKeyCompressed = EncodeECPointHexCompressed(pubKey);
+            Console.WriteLine("Public key (compressed): " + senderPubKeyCompressed);
+
+            string senderAddress = CalcRipeMD160(senderPubKeyCompressed);
+            Console.WriteLine("Blockchain address: " + senderAddress);
+
+            var tran = new
+            {
+                from = senderAddress,
+                to = recipientAddress,
+                value = value,
+                dateCreated = iso8601datetime,
+                senderPubKey = senderPubKeyCompressed,
+            };
+            string tranJson = JsonConvert.SerializeObject(tran);
+            Console.WriteLine("Transaction (JSON): {0}", tranJson);
+
+            byte[] tranHash = CalcSHA256(tranJson);
+            Console.WriteLine("Transaction hash(sha256): {0}", BytesToHex(tranHash));
+
+            BigInteger[] tranSignature = SignData(privateKey, tranHash);
+            Console.WriteLine("Transaction signature: [{0}, {1}]",
+                tranSignature[0].ToString(16), tranSignature[1].ToString(16));
+
+            var tranSigned = new
+            {
+                from = senderAddress,
+                to = recipientAddress,
+                value = value,
+                dateCreated = iso8601datetime,
+                senderPubKey = senderPubKeyCompressed,
+                senderSignature = new string[]
+                {
+                tranSignature[0].ToString(16),
+                tranSignature[1].ToString(16)
+                }
+            };
+
+            string signedTranJson = JsonConvert.SerializeObject(tranSigned, Formatting.Indented);
+            Console.WriteLine("Signed transaction (JSON):");
+            Console.WriteLine(signedTranJson);
+        }
+
+
+        static void Main(string[] args)
+        {
+            RandomPrivateKeyToAddress();
+
+            Console.WriteLine();
+            ExistingPrivateKeyToAddress("a1d15353e7dba1c2271c68bd4ea58032af8b46ce93d5b2354587f5ce58139c8e");
+
+            CreateAndSignTransaction(
+                recipientAddress: "f51362b7351ef62253a227a77751ad9b2302f911", value: 25,
+                iso8601datetime: "2018-02-10T17:53:48.972Z",
+                senderPrivKeyHex: "7e4670ae70c98d24f3662c172dc510a085578b9ccc717e6c2f4e547edd960a34");
+        }
+    }
+}
+
--- Faucet/Faucet/Faucet.Web/Controllers/HomeController.cs
+++ Faucet/Faucet/Faucet.Web/Controllers/HomeController.cs
@@ -1,24 +1,33 @@
-﻿using Faucet.Web.ViewModels;
-using Newtonsoft.Json.Linq;
-using Secp256k1;
-using System;
-using System.Globalization;
-using System.IO;
-using System.Net;
-using System.Numerics;
-using System.Security.Cryptography;
-using System.Text;
+﻿using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Crypto.Digests;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Asn1.Ocsp;
+using Org.BouncyCastle.Crypto.Signers;
 using System.Web.Mvc;
+using Faucet.Web.ViewModels;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Crypto.Parameters;
+using System.Text;
+using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Asn1.Sec;
+using System.Linq;
+using System;
+using System.Net;
+using System.IO;
 
 namespace Faucet.Web.Controllers
 {
     public class HomeController : Controller
     {
+        static readonly X9ECParameters curve = SecNamedCurves.GetByName("secp256k1");
+
         public ActionResult Index()
         {
             return View();
         }
 
+        /***********  approach 1 using Bouncy Castle    *************/
         [HttpPost]
         public ActionResult Index(TransactionViewModel transaction)
         {
@@ -30,117 +39,150 @@ namespace Faucet.Web.Controllers
 
             string nodeIpAddress = "127.0.0.1:5000";
             string newTransactionPostUrl = "/transactions/new";
-            string privateKeyStr = "cUgAvxseC9hpHXMuZkW1yMkTdijEcDTeLeMgmhaTd1eGnbMLdPpz";
+            string privateKeyStr = "7e4670ae70c98d24f3662c172dc510a085578b9ccc717e6c2f4e547edd960a34";
 
-            //source: https://github.com/TangibleCryptography/Secp256k1
+            string signedTransObject = CreateAndSignTransaction(transaction.To, 3, "2018-02-10T17:53:48.972Z", privateKeyStr);
+            ///* post data template
+            // * {
+            // * "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
+            // * "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
+            // * "value": 25.00,
+            // * "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f…eae1",
+            // * "senderSignature": ["e20c…a3c29d3370f79f", "cf92…0acd0c132ffe56"]
+            // * }
+            // */
 
-            string privKeyHex = SHA1HashStringForUTF8String(privateKeyStr);
-
-            BigInteger privateKey = Hex.HexToBigInteger(privKeyHex);
-            Secp256k1.ECPoint publicKey = Secp256k1.Secp256k1.G.Multiply(privateKey);
-
-            string bitcoinAddressCompressed = publicKey.GetBitcoinAddress(compressed: true);
-            uint value = 268435263;
-
-            var varInt = new VarInt(value); 
-            value = value ^ 2;
-
-            byte[] varIntBytes = VarInt.Encode(value);
-
-            // encryption
-            ECEncryption encryption = new ECEncryption();
-            const string message = "This is my encrypted message";
-            byte[] encrypted = encryption.Encrypt(publicKey, message);
-            byte[] decrypted = encryption.Decrypt(privateKey, encrypted);
-            string decryptedMessage = Encoding.UTF8.GetString(decrypted);
-
-            // signing
-            MessageSignerVerifier messageSigner = new MessageSignerVerifier();
-            SignedMessage signedMessage = messageSigner.Sign(privateKey, "Test Message to sign, you can verify this on http://brainwallet.org/#verify");
-            bool verified = messageSigner.Verify(signedMessage);
-
-            /* post data template
-             * {
-             * "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
-             * "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
-             * "value": 25.00,
-             * "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f…eae1",
-             * "senderSignature": ["e20c…a3c29d3370f79f", "cf92…0acd0c132ffe56"]
-             * }
-             */
-
-            WebResponse response = null;
-            HttpStatusCode statusCode = HttpStatusCode.RequestTimeout;
-
-            WebRequest request = WebRequest.Create(nodeIpAddress + newTransactionPostUrl);
-            statusCode = HttpStatusCode.RequestTimeout;
-
-            request.Method = "POST";
-            request.Timeout = 3000;
-            request.ContentType = "application/json; charset=utf-8";
-
-            JObject body = JObject.FromObject(new
+            try
             {
-                from = bitcoinAddressCompressed,  
-                to = transaction.To,
-                value = transaction.Value,
-                senderPublicKey = publicKey,
-                senderSignature = signedMessage.Signature,
-                nonce = 0
-            });
+                WebResponse response = null;
+                HttpStatusCode statusCode = HttpStatusCode.RequestTimeout;
 
-            Byte[] trans = Encoding.UTF8.GetBytes(body.ToString());
-            
-            Stream dataStream =request.GetRequestStream();
-            dataStream = request.GetRequestStream();
-            dataStream.Write(trans, 0, trans.Length);
-            dataStream.Close();
+                WebRequest request = WebRequest.Create(nodeIpAddress + newTransactionPostUrl);
+                statusCode = HttpStatusCode.RequestTimeout;
 
-            response = request.GetResponse();
-            statusCode = ((HttpWebResponse)response).StatusCode;
+                request.Method = "POST";
+                request.Timeout = 3000;
+                request.ContentType = "application/json; charset=utf-8";
 
-            /* response data template
-             * {
-             * "dateReceived": "2018-02-01T23:17:02.744Z",
-             * "transactionHash": "cd8d9a345bb208c6f9b8acd6b8eefe6…20c8a"
-             *  }
-             */
+                Byte[] trans = Encoding.UTF8.GetBytes(signedTransObject);
 
-            transaction.MessageResponse = statusCode.ToString() + ": " +  ((HttpWebResponse)response).StatusDescription;
-            transaction.TransactionHash = "pdasd3334t45fffdllozn44mja99qjjiuo2ygfytf4551ug44eqrtf2";// todo: display the transaction hash from the response
+                Stream dataStream = request.GetRequestStream();
+                dataStream = request.GetRequestStream();
+                dataStream.Write(trans, 0, trans.Length);
+                dataStream.Close();
+
+                response = request.GetResponse();
+                statusCode = ((HttpWebResponse)response).StatusCode;
+
+                /* response data template
+                 * {
+                 * "dateReceived": "2018-02-01T23:17:02.744Z",
+                 * "transactionHash": "cd8d9a345bb208c6f9b8acd6b8eefe6…20c8a"
+                 *  }
+                 */
+
+                transaction.MessageResponse = statusCode.ToString() + ": " + ((HttpWebResponse)response).StatusDescription;
+                transaction.TransactionHash = "TransactionHash: pdasd3334t45fffdllozn44mja99qjjiuo2ygfytf4551ug44eqrtf2";// todo: display the transaction hash from the response
+            }
+            catch (Exception ex)
+            {
+                transaction.MessageResponse = ex.Message;
+            }
 
             return View(transaction);
+
         }
 
-        /// <summary>
-        /// Compute hash for string encoded as UTF8
-        /// </summary>
-        /// <param name="s">String to be hashed</param>
-        /// <returns>40-character hex string</returns>
-        public string SHA1HashStringForUTF8String(string s)
+        private string CreateAndSignTransaction(string recipientAddress, int value,
+            string iso8601datetime, string senderPrivKeyHex)
         {
-            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            BigInteger privateKey = new BigInteger(senderPrivKeyHex, 16);
 
-            var sha1 = SHA1.Create();
-            byte[] hashBytes = sha1.ComputeHash(bytes);
+            ECPoint pubKey = GetPublicKeyFromPrivateKey(privateKey);
+            string senderPubKeyCompressed = EncodeECPointHexCompressed(pubKey);
 
-            return HexStringFromBytes(hashBytes);
-        }
+            string senderAddress = CalcRipeMD160(senderPubKeyCompressed);
 
-        /// <summary>
-        /// Convert an array of bytes to a string of hex digits
-        /// </summary>
-        /// <param name="bytes">array of bytes</param>
-        /// <returns>String of hex digits</returns>
-        public static string HexStringFromBytes(byte[] bytes)
-        {
-            var sb = new StringBuilder();
-            foreach (byte b in bytes)
+            var tran = new
             {
-                var hex = b.ToString("x2");
-                sb.Append(hex);
-            }
-            return sb.ToString();
+                from = senderAddress,
+                to = recipientAddress,
+                value = value,
+                dateCreated = iso8601datetime,
+                senderPubKey = senderPubKeyCompressed,
+            };
+            string tranJson = JsonConvert.SerializeObject(tran);
+
+            byte[] tranHash = CalcSHA256(tranJson);
+
+            BigInteger[] tranSignature = SignData(privateKey, tranHash);
+
+            var tranSigned = new
+            {
+                from = senderAddress,
+                to = recipientAddress,
+                value = value,
+                dateCreated = iso8601datetime,
+                senderPubKey = senderPubKeyCompressed,
+                senderSignature = new string[]
+                {
+                tranSignature[0].ToString(16),
+                tranSignature[1].ToString(16)
+                }
+            };
+
+            return JsonConvert.SerializeObject(tranSigned, Formatting.Indented);
+        }
+
+        public static ECPoint GetPublicKeyFromPrivateKey(BigInteger privKey)
+        {
+            ECPoint pubKey = curve.G.Multiply(privKey).Normalize();
+            return pubKey;
+        }
+
+        public static string EncodeECPointHexCompressed(ECPoint point)
+        {
+            BigInteger x = point.XCoord.ToBigInteger();
+            return x.ToString(16) + Convert.ToInt32(!x.TestBit(0));
+        }
+
+        private static string CalcRipeMD160(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            RipeMD160Digest digest = new RipeMD160Digest();
+            digest.BlockUpdate(bytes, 0, bytes.Length);
+            byte[] result = new byte[digest.GetDigestSize()];
+            digest.DoFinal(result, 0);
+            return BytesToHex(result);
+        }
+
+        public static string BytesToHex(byte[] bytes)
+        {
+            return string.Concat(bytes.Select(b => b.ToString("x2")));
+        }
+
+        /// <summary>
+        /// Calculates deterministic ECDSA signature (with HMAC-SHA256), based on secp256k1 and RFC-6979.
+        /// </summary>
+        private static BigInteger[] SignData(BigInteger privateKey, byte[] data)
+        {
+            ECDomainParameters ecSpec = new ECDomainParameters(curve.Curve, curve.G, curve.N, curve.H);
+            ECPrivateKeyParameters keyParameters = new ECPrivateKeyParameters(privateKey, ecSpec);
+            IDsaKCalculator kCalculator = new HMacDsaKCalculator(new Sha256Digest());
+            ECDsaSigner signer = new ECDsaSigner(kCalculator);
+            signer.Init(true, keyParameters);
+            BigInteger[] signature = signer.GenerateSignature(data);
+            return signature;
+        }
+
+        private static byte[] CalcSHA256(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            Sha256Digest digest = new Sha256Digest();
+            digest.BlockUpdate(bytes, 0, bytes.Length);
+            byte[] result = new byte[digest.GetDigestSize()];
+            digest.DoFinal(result, 0);
+            return result;
         }
 
         private bool ValidInputs(TransactionViewModel transaction)
@@ -155,5 +197,134 @@ namespace Faucet.Web.Controllers
 
             return true;
         }
+
+        /*              *********** approach 2 USING Secp256k1 Library ***********
+         * 
+         * 
+         * 
+        //[HttpPost]
+        //public ActionResult Index(TransactionViewModel transaction)
+        //{
+        //    if (!ValidInputs(transaction))
+        //    {
+        //        transaction.MessageResponse = "Invalid transaction details. Please check your details and try again.";
+        //        return View(transaction);
+        //    }
+
+        //    string nodeIpAddress = "127.0.0.1:5000";
+        //    string newTransactionPostUrl = "/transactions/new";
+        //    string privateKeyStr = "cUgAvxseC9hpHXMuZkW1yMkTdijEcDTeLeMgmhaTd1eGnbMLdPpz";
+
+        //    //source: https://github.com/TangibleCryptography/Secp256k1
+
+        //    string privKeyHex = SHA1HashStringForUTF8String(privateKeyStr);
+
+        //    BigInteger privateKey = Hex.HexToBigInteger(privKeyHex);
+        //    Secp256k1.ECPoint publicKey = Secp256k1.Secp256k1.G.Multiply(privateKey);
+
+        //    string bitcoinAddressCompressed = publicKey.GetBitcoinAddress(compressed: true);
+        //    uint value = 268435263;
+
+        //    var varInt = new VarInt(value); 
+        //    value = value ^ 2;
+
+        //    byte[] varIntBytes = VarInt.Encode(value);
+
+        //    // encryption
+        //    ECEncryption encryption = new ECEncryption();
+        //    const string message = "This is my encrypted message";
+        //    byte[] encrypted = encryption.Encrypt(publicKey, message);
+        //    byte[] decrypted = encryption.Decrypt(privateKey, encrypted);
+        //    string decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+        //    // signing
+        //    MessageSignerVerifier messageSigner = new MessageSignerVerifier();
+        //    SignedMessage signedMessage = messageSigner.Sign(privateKey, "Test Message to sign, you can verify this on http://brainwallet.org/#verify");
+        //    bool verified = messageSigner.Verify(signedMessage);
+
+        //    post data template
+        //     * {
+        //     * "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
+        //     * "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
+        //     * "value": 25.00,
+        //     * "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f…eae1",
+        //     * "senderSignature": ["e20c…a3c29d3370f79f", "cf92…0acd0c132ffe56"]
+        //     * }
+        //     
+
+        //    WebResponse response = null;
+        //    HttpStatusCode statusCode = HttpStatusCode.RequestTimeout;
+
+        //    WebRequest request = WebRequest.Create(nodeIpAddress + newTransactionPostUrl);
+        //    statusCode = HttpStatusCode.RequestTimeout;
+
+        //    request.Method = "POST";
+        //    request.Timeout = 3000;
+        //    request.ContentType = "application/json; charset=utf-8";
+
+        //    JObject body = JObject.FromObject(new
+        //    {
+        //        from = bitcoinAddressCompressed,  
+        //        to = transaction.To,
+        //        value = transaction.Value,
+        //        senderPublicKey = publicKey,
+        //        senderSignature = signedMessage.Signature,
+        //        nonce = 0
+        //    });
+
+        //    Byte[] trans = Encoding.UTF8.GetBytes(body.ToString());
+
+        //    Stream dataStream =request.GetRequestStream();
+        //    dataStream = request.GetRequestStream();
+        //    dataStream.Write(trans, 0, trans.Length);
+        //    dataStream.Close();
+
+        //    response = request.GetResponse();
+        //    statusCode = ((HttpWebResponse)response).StatusCode;
+
+        //     response data template
+        //     * {
+        //     * "dateReceived": "2018-02-01T23:17:02.744Z",
+        //     * "transactionHash": "cd8d9a345bb208c6f9b8acd6b8eefe6…20c8a"
+        //     *  }
+        //     
+
+        //    transaction.MessageResponse = statusCode.ToString() + ": " +  ((HttpWebResponse)response).StatusDescription;
+        //    transaction.TransactionHash = "pdasd3334t45fffdllozn44mja99qjjiuo2ygfytf4551ug44eqrtf2";// todo: display the transaction hash from the response
+
+        //    return View(transaction);
+        //}
+
+        ///// <summary>
+        ///// Compute hash for string encoded as UTF8
+        ///// </summary>
+        ///// <param name="s">String to be hashed</param>
+        ///// <returns>40-character hex string</returns>
+        //public string SHA1HashStringForUTF8String(string s)
+        //{
+        //    byte[] bytes = Encoding.UTF8.GetBytes(s);
+
+        //    var sha1 = SHA1.Create();
+        //    byte[] hashBytes = sha1.ComputeHash(bytes);
+
+        //    return HexStringFromBytes(hashBytes);
+        //}
+
+        ///// <summary>
+        ///// Convert an array of bytes to a string of hex digits
+        ///// </summary>
+        ///// <param name="bytes">array of bytes</param>
+        ///// <returns>String of hex digits</returns>
+        //public static string HexStringFromBytes(byte[] bytes)
+        //{
+        //    var sb = new StringBuilder();
+        //    foreach (byte b in bytes)
+        //    {
+        //        var hex = b.ToString("x2");
+        //        sb.Append(hex);
+        //    }
+        //    return sb.ToString();
+        //}
+        */
     }
 }
