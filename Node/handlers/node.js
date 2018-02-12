dbdfc89e2e03e14218d5a4da544f264f573d0ace
Milen Radkov <milenradkov@me.com>
2018-02-12 11:36:49 +0200

    Node synchronized with Fuacet
    Node synchronized with Fuacet

--- Node/handlers/node.js
+++ Node/handlers/node.js
@@ -5,124 +5,51 @@ let blockchain = require('./blockchain');
 let main = require('../index');
 let CryptoJS = require("crypto-js");
 
+let EC = require('elliptic').ec;
+let ec = new EC('secp256k1');
 
+//done
 module.exports.index = (req, res) => {
     res.send('SoftUni Chain Blockchain Node')
 }
 
+// TODO: confirmed transactions count, commulativedifficulty
 module.exports.getNodeInfo = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(
         {
             "about": "SoftUniChain/0.9-csharp",
-            "name": "Sofia-01",
-            "peers": 2,
-            "blocks": 25,
+            "nodeName": "Sofia-01",
+            "peers": main.peers,
+            "difficulty": main.difficulty,
+            "blocks": main.blockchain.length,
+            "cummulativeDifficulty": 127,
             "confirmedTransactions": 208,
-            "pendingTransactions": 7,
-            "addresses": 12,
-            "coins": 18000000
+            "pendingTransactions": main.pendingTransactions.length,
         }
     )
 }
 
+//done
 module.exports.getNodeBlocks = (req,res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(main.blockchain)
-
-        // [
-        // {
-        //     "index": 0,
-        //     "transactions": [
-        //         {
-        //             "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
-        //             "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
-        //             "value": 25.00,
-        //             "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
-        //             "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
-        //             "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
-        //             "paid": true,
-        //             "dateReceived": "2018-02-01T07:47:51.982Z",
-        //             "minedInBlockIndex": 7
-        //         },
-        //         {
-        //             "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
-        //             "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
-        //             "value": 25.00,
-        //             "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
-        //             "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
-        //             "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
-        //             "paid": true,
-        //             "dateReceived": "2018-02-01T07:47:51.982Z",
-        //             "minedInBlockIndex": 7
-        //         }
-        //     ],
-        //     "difficulty": 5,
-        //     "prevBlockHash": "d279fa6a31ae4fb07cfd9cf7f35cc01f�3cf20a",
-        //     "minedBy": "f582d57711a618e69d588ce93895d749858fa95b",
-        //     "blockDataHash": "5d845cddcd4404ecfd5476fd6b1cf0e5�a80cd3",
-        //     "nonce": 2455432,
-        //     "dateCreated": "2018-02-01T23:23:56.337Z",
-        //     "blockHash": "00000abf2f3d86d5c000c0aa7a425a6a4a65�cf4c"
-        // },
-        //     {
-        //         "index": 1,
-        //         "transactions": [],
-        //         "difficulty": 5,
-        //         "prevBlockHash": "d279fa6a31ae4fb07cfd9cf7f35cc01f�3cf20a",
-        //         "minedBy": "f582d57711a618e69d588ce93895d749858fa95b",
-        //         "blockDataHash": "5d845cddcd4404ecfd5476fd6b1cf0e5�a80cd3",
-        //         "nonce": 2455432,
-        //         "dateCreated": "2018-02-01T23:23:56.337Z",
-        //         "blockHash": "00000abf2f3d86d5c000c0aa7a425a6a4a65�cf4c"
-        //     }
-        // ]
 }
 
+//done
 module.exports.getNodeBlockByIndex = (req,res) => {
     let index = req.params.index;
     res.setHeader('Content-Type', 'application/json');
     res.send(main.blockchain[index]);
-
-    // {
-    //     "index":index,
-    //     "transactions": [
-    //     {
-    //         "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
-    //         "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
-    //         "value": 25.00,
-    //         "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
-    //         "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
-    //         "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
-    //         "paid": true,
-    //         "dateReceived": "2018-02-01T07:47:51.982Z",
-    //         "minedInBlockIndex": 7
-    //     },
-    //     {
-    //         "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
-    //         "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
-    //         "value": 25.00,
-    //         "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
-    //         "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
-    //         "transactionHash": "4dfc3e0ef89ed603ed54e47435a18b836b�176a",
-    //         "paid": true,
-    //         "dateReceived": "2018-02-01T07:47:51.982Z",
-    //         "minedInBlockIndex": 7
-    //     }
-    // ],
-    //     "difficulty": 5,
-    //     "prevBlockHash": "d279fa6a31ae4fb07cfd9cf7f35cc01f�3cf20a",
-    //     "minedBy": "f582d57711a618e69d588ce93895d749858fa95b",
-    //     "blockDataHash": "5d845cddcd4404ecfd5476fd6b1cf0e5�a80cd3",
-    //     "nonce": 2455432,
-    //     "dateCreated": "2018-02-01T23:23:56.337Z",
-    //     "blockHash": "00000abf2f3d86d5c000c0aa7a425a6a4a65�cf4c"
-    // }
 }
 
+// TODO: correct balance data by address
 module.exports.getNodeBalanceByAddress = (req,res) => {
     let address = req.params['address'];
     let confirmCount = parseInt(req.params['confirmCount']);
+
+    // TODO: send correct data
+
     res.setHeader('Content-Type', 'application/json');
     res.send(
         {
@@ -136,6 +63,12 @@ module.exports.getNodeBalanceByAddress = (req,res) => {
 
 module.exports.postNewTransaction = (req,res) => {
     let newTransaction = req.body;
+
+    // TODO : VERSION 2 VERIFY TRANSACTION SIGNATURE
+    // let key = ec.keyFromPublic(newTransaction.senderPubKey, 'hex');
+    // let verify = key.verify(newTransaction.transactionHash, newTransaction.senderSignature);
+
+    let timestamp = new Date(newTransaction.dateCreated).getTime();
     let transactionHash = CryptoJS.SHA256(newTransaction.from + newTransaction.to + newTransaction.value + newTransaction.senderPubKey + newTransaction.senderSignature + newTransaction.timestamp).toString();
 
     main.pendingTransactions.push(newTransaction);
@@ -143,12 +76,13 @@ module.exports.postNewTransaction = (req,res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(
         {
-            "dateReceived": Date.now(),
+            "dateReceived": new Date().toString(),
             "transactionHash": transactionHash
         }
     )
 }
 
+//done
 module.exports.getMiningBlock = (req, res) => {
     let minerAddress = req.params['address'];
     let miningJob = blockchain.miningJob(minerAddress);
@@ -157,6 +91,7 @@ module.exports.getMiningBlock = (req, res) => {
     res.send(miningJob)
 }
 
+// TODO: send correct data
 module.exports.getTransactionInfo = (req,res) => {
     let tranHash = req.params['tranHash'];
     res.setHeader('Content-Type', 'application/json');
@@ -165,8 +100,8 @@ module.exports.getTransactionInfo = (req,res) => {
             "from": "44fe0696beb6e24541cc0e8728276c9ec3af2675",
             "to": "9a9f082f37270ff54c5ca4204a0e4da6951fe917",
             "value": 25.00,
-            "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4f�eae1",
-            "senderSignature": ["e20c�a3c29d3370f79f", "cf92�0acd0c132ffe56"],
+            "senderPubKey": "2a1d79fb8743d0a4a8501e0028079bcf82a4feae1",
+            "senderSignature": ["e20ca3c29d3370f79f", "cf920acd0c132ffe56"],
             "transactionHash": tranHash,
             "paid": true,
             "dateReceived": "2018-02-01T07:47:51.982Z",
@@ -175,6 +110,7 @@ module.exports.getTransactionInfo = (req,res) => {
     )
 }
 
+//done
 module.exports.newBlockNotify = (req,res) => {
     let blockIndex = parseInt(req.body.index);
     res.setHeader('Content-Type', 'application/json');
@@ -185,18 +121,22 @@ module.exports.newBlockNotify = (req,res) => {
     )
 }
 
+//done
 module.exports.getAllPeers = (req,res) => {
+
+    // Return all known peers
+
     res.setHeader('Content-Type', 'application/json');
-    res.send(
-        [
-            "http://212.50.11.109:5555",
-            "http://af6c7a.ngrok.org:5555"
-        ]
-    )
+    res.send(main.peers);
 }
 
+//done
 module.exports.postNewPeer = (req,res) => {
     let nodeUrl = req.body.url;
+
+    // Add new peer.
+    main.peers.push(nodeUrl);
+
     res.setHeader('Content-Type', 'application/json');
     res.send(
         {
@@ -205,10 +145,16 @@ module.exports.postNewPeer = (req,res) => {
     )
 }
 
-//Receive mining job done from miner
+//done
 module.exports.postPOW = (req,res) => {
+    //Receive mining job done from miner and assert it
     let jobDone = blockchain.postPOW(req.body);
     res.setHeader('Content-Type', 'application/json');
     res.send(jobDone)
+}
+
+module.exports.getPendingTransactiosn = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(main.pendingTransactions);
 }
 
